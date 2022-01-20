0cc1355e0743e5667c3c79eea938f4d64575479c
ichxxx <czn16@qq.com>
2022-01-20 16:06:31 +0800

    [+] add DecodeBase64

--- base64.go
+++ base64.go
@@ -5,11 +5,21 @@ import (
 )
 
 func Base64(s string) string {
-	length := len(s)/3
-	if length*3 != len(s) {
-		length++
+	x := len(s)/3
+	if x*3 != len(s) {
+		x++
 	}
-	buf := make([]byte, length*4)
+	buf := make([]byte, x*4)
 	base64.StdEncoding.Encode(buf, UnsafeBytes(s))
+	return UnsafeString(buf)
+}
+
+func DecodeBase64(s string) string {
+	blank := 0
+	for i := len(s)-1; i >= 0 && s[i] == '='; i-- {
+		blank++
+	}
+	buf := make([]byte, (len(s)/4)*3-blank)
+	_, _ = base64.StdEncoding.Decode(buf, UnsafeBytes(s))
 	return UnsafeString(buf)
 }
--- base64_test.go
+++ base64_test.go
@@ -15,6 +15,15 @@ func TestBase64(t *testing.T) {
 	}
 }
 
+func TestDecodeBase64(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		b64 := base64.StdEncoding.EncodeToString([]byte(strings.Repeat("1", i)))
+		if expected, _ := base64.StdEncoding.DecodeString(b64); DecodeBase64(b64) != string(expected) {
+			t.Fatal()
+		}
+	}
+}
+
 func BenchmarkRawBase64Short(b *testing.B) {
 	input := "123456789"
 	b.ReportAllocs()
@@ -48,5 +57,41 @@ func BenchmarkBase64Long(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		Base64(input)
+	}
+}
+
+func BenchmarkRawDecodeBase64Short(b *testing.B) {
+	input := Base64("123456789")
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		base64.StdEncoding.DecodeString(input)
+	}
+}
+
+func BenchmarkRawDecodeBase64Long(b *testing.B) {
+	input := Base64(strings.Repeat("123456789", 1024))
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		base64.StdEncoding.DecodeString(input)
+	}
+}
+
+func BenchmarkDecodeBase64Short(b *testing.B) {
+	input := Base64("123456789")
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		DecodeBase64(input)
+	}
+}
+
+func BenchmarkDecodeBase64Long(b *testing.B) {
+	input := Base64(strings.Repeat("123456789", 1024))
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		DecodeBase64(input)
 	}
 }
